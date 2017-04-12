2c08f786500e893799b624390f35c10ee7a96b78
Richard Moss <richard@englishtutorslive.com>
2017-04-12 12:48:07 +0100

    Initial commit, corrects path to foundation in webpack.config

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
